34a6424607eb3da6016db8af8823482b0852456d
slipper <slipper@0ops.net>
2021-04-27 14:21:56 +0800

    implement isOdd api

--- service/src/service.c
+++ service/src/service.c
@@ -1,37 +1,74 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+// idea from https://isevenapi.xyz/
 
 int main(int argc, char** argv) {
-    printf("awesome chall\n");
-    fprintf(stderr, "this is stderr\n");
-    printf("who are you?\n");
-    fflush(stdout);
+    const char *path = "GET /api/isodd/";
+    const char *ad = "Buy isOddCoin, the hottest new cryptocurrency!";
+    char number[0x20] = {0};
+    char buf[0x400] = {0};
+    int n = read(0, buf, sizeof(buf) - 1);
+    if (n > sizeof(path) && strncmp(&buf[0], path, 15) == 0) {
+        char *endl = strchr(buf, '\n');
+        if (endl != NULL) {
+            *endl = '\0';
+            if (*--endl == '\r') {
+                *endl = '\0';
+            }
+        }
+        endl = strstr(buf, " HTTP/");
+        if (endl != NULL) {
+            *endl = '\0';
+        }
 
-    char* line = NULL;
-    size_t num_allocated;
-    size_t num_read = getline(&line, &num_allocated, stdin);
+        char *token = "public";
+        char *query = strchr(buf, '?');
+        if (query != NULL) {
+            *query = '\0';
+            query++;
+            if (!strncmp(query, "token=", 6)) {
+                token = query + 6;
+            }
+        }
+        const char *request = &buf[15];
+        char *slash = strchr(request, '/');
+        if (slash != NULL) {
+            *slash = '\0';
+        }
 
-    if (num_read > 0) {
-        line[num_read - 1] = '\0';
-
-        if (*line == '/') {
-            char buf[1024];
-            FILE* input = fopen(line, "r");
-            if (input != NULL) {
-                ssize_t num_file_read = fread(buf, 1, sizeof(buf), input);
-                if (num_file_read > 0) {
-                    buf[num_file_read - 1] = '\0';
-                    printf("%s\n", buf);
-                }
-
-                fclose(input);
+        size_t n = strlen(request);
+        if (!strcmp(token, "enterprise")) {
+        } else if (!strcmp(token, "premium")) {
+            if (n > 9) {
+                puts("{\n\t\"error\": \"sign up for enterprise to get large number support\"\n}");
+                return 0;
             }
         } else {
-            printf("you said %s\n", line);
+            token = "public";
+            if (n > 6) {
+                puts("{\n\t\"error\": \"sign up for premium or enterprise to get large number support\"\n}");
+                return 0;
+            }
+        }
+        memcpy(&number, request, n);
+        number[n] = 0;
+
+        if (number[0] == '-' && !strcmp(token, "public")) {
+            puts("{\n\t\"error\": \"sign up for premium or enterprise to get negative number support\"\n}");
+            return 0;
+        } else {
+            int last_digit = number[n - 1] - '0';
+            if (!strcmp(token, "public")) {
+                printf("{\n\t\"isodd\": %s,\n\t\"ad\": \"%s\"\n}", (last_digit % 2 == 1) ? "true": "false", ad);
+            } else {
+                printf("{\n\t\"isodd\": %s\n}", (last_digit % 2 == 1) ? "true": "false");
+            }
+            return 0;
         }
     }
-
-    fflush(stdout);
-    free(line);
     return 0;
 }
