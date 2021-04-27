dc831953c14611c45179fdf353f07094d1da99bc
slipper <slipper@0ops.net>
2021-04-27 14:33:51 +0800

    update challenge info

--- interaction/check1.sh
+++ interaction/check1.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-RESULT=$(echo "adam" | nc "$1" "$2")
+RESULT=$(curl "http://$1:$2/api/isodd/1337/")
 
-echo "$RESULT" | grep "awesome chall"
-echo "$RESULT" | grep "who are you?"
-echo "$RESULT" | grep "you said adam"
+echo "$RESULT" | grep "isodd"
+echo "$RESULT" | grep "true"
+echo "$RESULT" | grep "ad"
 
